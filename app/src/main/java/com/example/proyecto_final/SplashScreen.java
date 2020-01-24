cc5b890beee00a505d29a3cf61b542a9b6bab22c
LeydiHuallpa <leydihuallpa67@gmail.com>
2020-01-24 12:59:58 -0500

    Modificacion del Splash

--- app/src/main/java/com/example/proyecto_final/SplashScreen.java
+++ app/src/main/java/com/example/proyecto_final/SplashScreen.java
@@ -9,10 +9,18 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
 
+import com.felipecsl.gifimageview.library.GifImageView;
+
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 
 public class SplashScreen extends Activity {
     private final int DURACION_SPLASH = 5000;
 
+    private GifImageView gifImageView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,13 +30,27 @@ public class SplashScreen extends Activity {
 
         setContentView(R.layout.activity_splash_screen);
 
-        new Handler().postDelayed(new Runnable(){
-            public void run(){
-                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
-                startActivity(intent);
-                finish();
-            };
-        }, DURACION_SPLASH);
+        gifImageView = (GifImageView)findViewById(R.id.gifImageView);
+
+        //Set GIFImageView resource
+        try{
+            InputStream inputStream = getAssets().open("loading.gif");
+            byte[] bytes = IOUtils.toByteArray(inputStream);
+            gifImageView.setBytes(bytes);
+            gifImageView.startAnimation();
+        }
+        catch (IOException ex)
+        {
+
+        }
+        //Wait for 3 seconds and start Activity Main
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                SplashScreen.this.startActivity(new Intent(SplashScreen.this,MainActivity.class));
+                SplashScreen.this.finish();
+            }
+        },5000); // 3000 = 3seconds
     }
 
 }
