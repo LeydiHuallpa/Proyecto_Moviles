ebdd2e8c5103445629106be05e88a3afc181ef80
LeydiHuallpa <leydihuallpa67@gmail.com>
2020-01-24 12:08:59 -0500

    Implementacion del Splash

---
+++ app/src/main/java/com/example/proyecto_final/SplashScreen.java
@@ -0,0 +1,34 @@
+package com.example.proyecto_final;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+
+public class SplashScreen extends Activity {
+    private final int DURACION_SPLASH = 5000;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable(){
+            public void run(){
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            };
+        }, DURACION_SPLASH);
+    }
+
+}
