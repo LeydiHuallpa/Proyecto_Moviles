f6137b3708769d3ac90f820fae44e57164e8f53f
Stweer <38274756+Stweer@users.noreply.github.com>
2020-01-26 11:05:05 -0500

    Creacion de Layout SignUp

--- app/src/main/java/com/example/proyecto_final/Login/SignUp.java
+++ app/src/main/java/com/example/proyecto_final/Login/SignUp.java
@@ -2,21 +2,15 @@ package com.example.proyecto_final.Login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import com.example.proyecto_final.R;
 
-public class Login extends AppCompatActivity {
+public class SignUp extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-    }
-
-    public void Registro(View view) {
-        startActivity(new Intent(this,SignUp.class));
+        setContentView(R.layout.activity_sign_up);
     }
 }
